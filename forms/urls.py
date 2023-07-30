cbd13e3bf295cbcf139931811b605f01232877cc
san6138 <101883757+san6138@users.noreply.github.com>
2023-07-31 02:52:05 +0530

    Delete forms directory

--- forms/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.Indexview.as_view()),
-    path("register/", views.RegisterView.as_view()),
- #   path("savedata/", views.save_data)
-]
